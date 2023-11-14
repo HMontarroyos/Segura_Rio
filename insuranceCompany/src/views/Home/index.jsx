a88613d3c577c0ec0155942b4c70f2c41ee9bf11
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-11-14 17:10:48 -0300

    refactor: add view Home in insuranceCompany

---
+++ insuranceCompany/src/views/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./styled";
+import { Header } from "../../components";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <p>TESTETTETETETE</p>
+    </>
+  );
+};
+
+export default Home;
