d02b632dce289f5b787627c147e188b994d3332c
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-11-14 17:10:27 -0300

    feat: add component Header in insuranceCompany

---
+++ insuranceCompany/src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import * as S from "./styled";
+
+function Header() {
+  return (
+    <S.Container>
+      <S.Header>
+        <h1>Seguradora</h1>
+      </S.Header>
+    </S.Container>
+  );
+}
+
+export default Header;
---
+++ insuranceCompany/src/components/Header/styled.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  z-index: 9999;
+  width: 100%;
+  top: 0;
+`;
+
+export const Header = styled.header`
+  background-color: green;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 0.5rem 1rem;
+  height: 80px;
+`;
