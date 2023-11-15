9a1fbb71cbc4b6cfd1d3907ba8876f6764660ec6
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-11-15 16:55:00 -0300

    feat: add consts global in insuranceCompany front

---
+++ insuranceCompany/src/global/const.jsx
@@ -0,0 +1,8 @@
+export const descriptionSeguraRio = [
+  "Bem-vindo à Segura Rio, onde nossa missão é superar expectativas, protegendo o que é mais importante para você.",
+  "Oferecemos coberturas abrangentes que transcendem a mera proteção, proporcionando tranquilidade em todas as fases da vida.",
+  "Nossas coberturas são elaboradas para atender às necessidades específicas dos cariocas, refletindo a essência vibrante e única do Rio de Janeiro.",
+  "Na Segura Rio, não protegemos apenas bens; construímos laços duradouros de confiança.",
+  "Conheça nossas coberturas e descubra como podemos ser o parceiro confiável que caminha ao seu lado, enfrentando juntos os desafios do presente e garantindo um futuro mais seguro.",
+  "Sua proteção é nossa prioridade.",
+];
