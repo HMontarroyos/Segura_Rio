7cdc71190a08a9f1a7b15078d52b6805b063c6c0
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-11-15 16:55:20 -0300

    feat: add view Home in insuranceCompany front

--- insuranceCompany/src/views/Home/index.jsx
+++ insuranceCompany/src/views/Home/index.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import * as S from "./styled";
+import Fade from "react-reveal/Fade";
 import { Header } from "../../components";
+import { descriptionSeguraRio } from "../../global/const";
+import happy_woman from "../../assets/images/happy_woman.png";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <p>TESTETTETETETE</p>
+      <S.Container>
+        <S.ContainerText>
+          <S.Title>Segura Rio</S.Title>
+          {descriptionSeguraRio?.map((description, key) => (
+            <S.Paragraph key={key}>{description}</S.Paragraph>
+          ))}
+        </S.ContainerText>
+        <Fade right>
+          <S.Image src={happy_woman} alt={"Uma foto de uma mulher sorrindo"} />
+        </Fade>
+      </S.Container>
     </>
   );
 };
--- insuranceCompany/src/views/Home/styled.jsx
+++ insuranceCompany/src/views/Home/styled.jsx
@@ -1,1 +1,38 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  padding-top: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: left;
+  padding: 20px 0 0 20px;
+  background-color: #0069ff;
+`;
+
+export const ContainerText = styled.div`
+  width: 100%;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.h1`
+  color: white;
+  font-weight: bold;
+  font-size: 80px;
+`;
+
+export const Paragraph = styled.p`
+  color: white;
+  font-size: 18px;
+  letter-spacing: 1px;
+  gap: 20px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
+
+export const Image = styled.img`
+  max-width: 420px;
+  bottom: 0;
+  margin-top: auto;
+`;
