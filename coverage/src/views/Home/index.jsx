a96c6e9badff189908652b76d359f6c673cf4074
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-11-15 16:53:00 -0300

    feat: add functionality in home and coverage front

--- coverage/src/views/Home/index.jsx
+++ coverage/src/views/Home/index.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import * as S from "./styled";
+import { CardCoverage } from "../../components";
+import { coverages } from "../../global/const";
 
 const Home = () => {
   return (
-    <>
-      <p>Cobertura</p>
-    </>
+    <S.Container>
+      <S.Title>
+        Conheça as nossas coberturas e descubra a segurança que vai além das
+        expectativas.
+      </S.Title>
+      <S.ContainerCard>
+      {coverages?.map((coverage, key) => (
+        <CardCoverage name={coverage.name} icon={coverage.icon} key={key} />
+      ))}
+      </S.ContainerCard>
+    </S.Container>
   );
 };
 
