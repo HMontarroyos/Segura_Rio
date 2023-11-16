a29ab98e44c9553041fb047616aa4c696744f7d7
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-11-16 13:49:46 -0300

    feat: add components in Home in coverageInsurance

--- coverageInsuranceCompany/src/views/Home/index.jsx
+++ coverageInsuranceCompany/src/views/Home/index.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./styled";
-import { CardCoverage } from "../../components";
+import { CardCoverage, Modal } from "../../components";
 import { coverages } from "../../global/const";
 
 const Home = () => {
+  const [modalInfo, setModalInfo] = useState(null);
+
+  const handleCardClick = (info) => {
+    setModalInfo(info);
+  };
+
+  const closeModal = () => {
+    setModalInfo(null);
+  };
+
+  const closeModalAndScroll = () => {
+    closeModal();
+    const bottomPage = document.getElementById("form");
+    if (bottomPage) {
+      window.scrollTo({
+        top: bottomPage.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <S.Container>
       <S.Title>
@@ -11,10 +32,24 @@ const Home = () => {
         expectativas.
       </S.Title>
       <S.ContainerCard>
-      {coverages?.map((coverage, key) => (
-        <CardCoverage name={coverage.name} icon={coverage.icon} key={key} />
-      ))}
+        {coverages?.map((coverage, key) => (
+          <CardCoverage
+            name={coverage.name}
+            icon={coverage.icon}
+            description={coverage.description}
+            key={key}
+            onClick={handleCardClick}
+          />
+        ))}
       </S.ContainerCard>
+      <Modal
+        isOpen={!!modalInfo}
+        onClose={closeModal}
+        title={modalInfo?.title}
+        description={modalInfo?.description}
+        icon={modalInfo?.icon}
+        onButtonClick={closeModalAndScroll}
+      />
     </S.Container>
   );
 };
