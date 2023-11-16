df90f470b7825b2987677a46854a7b9f472a5211
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-11-16 18:23:39 -0300

    feat: add const in formInsuranceCompany

---
+++ formInsuranceCompany/src/global/const.jsx
@@ -0,0 +1,21 @@
+import { TbBeach, Tb24Hours } from "react-icons/tb";
+import { FaMoneyBillTrendUp } from "react-icons/fa6";
+
+
+export const benefits = [
+  {
+    name: "Carioca Plus",
+    icon: <TbBeach />,
+    description: "Como membro do 'Carioca Plus', os segurados da Segura Rio aproveitam descontos exclusivos em uma variedade de estabelecimentos parceiros, proporcionando benefícios além das apólices tradicionais.",
+  },
+  {
+    name: "Assistência 24 horas",
+    icon: <Tb24Hours/>,
+    description: "Os segurados da Segura Rio contam com uma assistência 24 horas para emergências, garantindo suporte imediato em casos de acidentes, panes ou situações imprevistas, proporcionando tranquilidade a qualquer hora do dia.",
+  },
+  {
+    name: "Descontos em Renovação",
+    icon: <FaMoneyBillTrendUp/>,
+    description: "Oferecemos descontos especiais para clientes que renovam suas apólices, reconhecendo e premiando a fidelidade dos segurados com benefícios financeiros significativos.",
+  }
+];
