9147aadd8b485b140a05efd88e3ad789436a2a66
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-11-16 08:45:16 -0300

    feat: add axios api mock in front ends and container

--- container/src/hmontarroyos-root-config.js
+++ container/src/hmontarroyos-root-config.js
@@ -1,20 +1,29 @@
 import { registerApplication, start } from "single-spa";
+import axios from 'axios';
 
-registerApplication({
-  name: "@hmontarroyos/insuranceCompany",
-  app: () => System.import("@hmontarroyos/insuranceCompany"),
-  activeWhen: (location) =>
-    location.pathname === "/",
-});
+async function fetchData() {
+  try {
+    const response = await axios.get('https://run.mocky.io/v3/39566f8f-1da0-47b4-952d-45b0a055188d');
+    const data = response.data;
 
-registerApplication({
-  name: "@hmontarroyos/coverageInsuranceCompany",
-  app: () => System.import("@hmontarroyos/coverageInsuranceCompany"),
-  activeWhen: (location) =>
-    location.pathname === "/",
-});
+    data.applications.forEach(app => {
+      registerApplication({
+        name: app.name,
+        app: () => System.import(app.package),
+        activeWhen: app.exact ? (location) => location.pathname === app.activeWhen : [app.activeWhen],
+      });
+    });
+  } catch (error) {
+    console.error('Error getting data:', error);
+  } finally {
+    start({ urlRerouteOnly: true });
+  }
+}
+
+fetchData();
 
 
-start({
-  urlRerouteOnly: true,
-});
+
+
+
+
