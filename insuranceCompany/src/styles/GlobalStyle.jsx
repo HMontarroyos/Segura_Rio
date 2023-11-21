6c63b1540bbb746ec74cd9cfcc1af977b12ac3d6
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-11-21 08:18:52 -0300

    style: adjust in home

--- insuranceCompany/src/styles/GlobalStyle.jsx
+++ insuranceCompany/src/styles/GlobalStyle.jsx
@@ -5,7 +5,8 @@ export const GlobalStyle = createGlobalStyle`
     border: 0;
     padding: 0;
     margin: 0;
-
+    overflow-x: hidden;
+    overflow-y: auto;
   }
 
   h1,h2,h3,h4,h5,h6 {
@@ -16,6 +17,8 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Exo 2', sans-serif;
   }
 
-  body {background-color:  #CCE1FF;}
+  body {
+    background-color:  #CCE1FF;
+  }
 
 `;
