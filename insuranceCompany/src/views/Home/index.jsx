1d76f10de3a76ab99a2785245093898951bda444
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-11-22 09:54:44 -0300

    feat: add politices and cokkies in lib react-cookie-consent and clarity in application

--- insuranceCompany/src/views/Home/index.jsx
+++ insuranceCompany/src/views/Home/index.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as S from "./styled";
 import Fade from "react-reveal/Fade";
+import CookieConsent from "react-cookie-consent";
 import { Header } from "../../components";
 import { descriptionSeguraRio } from "../../global/const";
 import happy_woman from "../../assets/images/happy_woman.png";
 
 const Home = () => {
+  const [showCookieConsent, setShowCookieConsent] = useState(false);
+
+  const onAcceptCookies = () => {
+    window.addEventListener("consentGranted", () => window.clarity("consent"));
+    setShowCookieConsent(false);
+  };
+
+  const onDeclineCookies = () => {};
+
+  useEffect(() => {
+    const acceptedCookie = document.cookie
+      .split(";")
+      .some((cookie) => cookie.trim().startsWith("acceptedCookie="));
+
+    if (!acceptedCookie) {
+      setShowCookieConsent(true);
+    }
+  }, []);
+
   return (
     <>
       <Header />
@@ -20,6 +40,45 @@ const Home = () => {
           <S.Image src={happy_woman} alt={"Uma foto de uma mulher sorrindo"} />
         </Fade>
       </S.Container>
+      {showCookieConsent && (
+        <CookieConsent
+          style={{ background: "#CCE1FF" }}
+          buttonStyle={{
+            background: "#0054cc",
+            color: "white",
+            fontWeight: "bold",
+            borderRadius: "5px",
+            padding: "10px",
+          }}
+          buttonText="Aceitar"
+          declineButtonText="Cancelar"
+          declineButtonStyle={{
+            background: "#BB0B0B",
+            color: "white",
+            fontWeight: "bold",
+            borderRadius: "5px",
+            padding: "10px",
+          }}
+          cookieName="acceptedCookie"
+          debug
+          overlay
+          enableDeclineButton
+          onAccept={onAcceptCookies}
+          onDecline={onDeclineCookies}
+        >
+          <p
+            style={{
+              fontSize: "16px",
+              color: "#003F99",
+            }}
+          >
+            Usamos cookies para armazenar informações sobre como você usa o
+            nosso site e as páginas que visita. Tudo para tornar sua experiência
+            a mais agradável possível. Ao clicar em 'Aceitar', você consente com
+            a utilização de cookies.
+          </p>
+        </CookieConsent>
+      )}
     </>
   );
 };
